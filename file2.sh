cd8986ab95415b5f8d96e2e607b7000b9ecd5d5b
tejayarra5566 <tejayarra99@gmail.com>
2025-04-22 07:09:24 +0530

    script added

---
+++ file2.sh
@@ -0,0 +1,43 @@
+mkdir naresh
+
+cd naresh
+
+echo hi file1
+
+mkdir teja
+
+cd teja
+
+echo hello teja
+
+yum install java -y
+
+yum install wget -y
+
+yum install unzip -y
+
+yum install vim -y
+
+yum install epel-release -y
+
+yum install ansible -y
+
+yum install git -y
+
+yum install maven -y
+
+yum install python -y
+
+yum install python-pip -y
+
+yum install tree -y
+
+yum install java -y
+
+yum install nginx -y
+
+ansible --version
+
+java -version
+
+echo successfully completed
